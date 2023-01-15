6fb3c5c4fea8b2c3c8a511b0b77360a6b112120c
miwanoff <marina.iva.noffa@gmail.com>
2023-01-15 12:51:29 +0200

    Add words adding

---
+++ drag-and-drop.js
@@ -0,0 +1,15 @@
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  //   event.dataTransfer.setData("text", "Ирис");
+  event.dataTransfer.setData("text", event.target.innerHTML);
+}
+
+function drop(event) {
+  event.preventDefault();
+  var drop = document.getElementById("word");
+  var data = event.dataTransfer.getData("text");
+  drop.innerHTML += data;
+}
